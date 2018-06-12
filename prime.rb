31a5c5e764739a22bb21ec6f1871dd8f22752e02
Valerie Moore <periwinkle17@outlook.com>
2018-06-12 15:31:00 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,10 @@ def prime?(integer)
   prime = true
   number_array = (2..integer-1).to_a
   number_array.each do |num|
-    if integer % num == 0 || integer % num < 0
+    if integer % num == 0
+      prime = false
+      end
+    if integer % num < 0
       prime = false
     end
   end
